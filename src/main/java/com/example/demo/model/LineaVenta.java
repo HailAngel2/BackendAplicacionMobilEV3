2c2cdd0d554e2fb88ed04dabd25854ec3e48c7c7
HailAngel2 <angelpimiento15@gmail.com>
2025-12-03 06:35:23 -0300

    añadido controller modificar contraseña

--- src/main/java/com/example/demo/model/LineaVenta.java
+++ src/main/java/com/example/demo/model/LineaVenta.java
@@ -9,11 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter @Setter
@@ -28,6 +30,8 @@ public class LineaVenta {
     @ManyToOne
     @JoinColumn(name = "idVenta")
     @JsonBackReference
+    @ToString.Exclude
+    @JsonIgnore
     private Venta venta;
     
     @ManyToOne
