33bcb8f4ed30dcdd3a3edb274406f7412e843c83
HailAngel2 <angelpimiento15@gmail.com>
2025-12-02 23:15:59 -0300

    añadido controller modificar contraseña

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.PasswordUpdateDTO;
 import com.example.demo.dto.RegistroRequestDTO;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +29,28 @@ public class UsuarioController {
     
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PatchMapping("/{id}/password")
+    public ResponseEntity<?> updatePassword(@PathVariable Long id, @RequestBody PasswordUpdateDTO passwordDTO) {
+        
+        // 1. Buscar usuario
+        Usuario usuario = usuarioService.getUsuarioById(id);
+        
+        if (usuario == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // 2. Encriptar y actualizar contraseña
+        usuario.setContrasena(passwordEncoder.encode(passwordDTO.getNuevoPassword()));
+        
+        // 3. Guardar cambios
+        usuarioService.updateUsuario(id, usuario);
+
+        return ResponseEntity.ok().body("{\"message\": \"Contraseña actualizada correctamente\"}");
+    }
 
     @GetMapping
     public ResponseEntity<List<Usuario>> getAll() {
