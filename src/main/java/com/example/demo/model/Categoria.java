f6311d81e9e84e5487b95b9486b302e66fc0eeb4
HailAngel2 <angelpimiento15@gmail.com>
2025-12-16 08:45:29 -0300

    añadido controller modificar contraseña

--- src/main/java/com/example/demo/model/Categoria.java
+++ src/main/java/com/example/demo/model/Categoria.java
@@ -1,11 +1,16 @@
 package com.example.demo.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +26,8 @@ public class Categoria{
 
     @Column(name = "nombreCategoria", length = 50, unique = true, nullable = false)
     private String nombreCategoria;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Producto> productos;
 }
